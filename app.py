c3398682f1a7194faee29bfa6df29f6b1b6f7518
Andrei Chirap <34674304+AndreiChirap@users.noreply.github.com>
2025-02-28 11:52:37 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ from rag_methods import (
     stream_llm_response,
     stream_llm_rag_response,
 )
-
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 dotenv.load_dotenv()
 
 if "AZ_OPENAI_API_KEY" not in os.environ:
@@ -33,6 +35,10 @@ else:
     MODELS = ["azure-openai/gpt-4o"]
 
 
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
 st.set_page_config(
     page_title="RAG LLM app?", 
     page_icon="📚", 
@@ -41,164 +47,184 @@ st.set_page_config(
 )
 
 
-# --- Header ---
-st.html("""<h2 style="text-align: center;">📚🔍 <i> Welcome to LATCHBOT!</i> 🤖💬</h2>""")
+# --- Authentication ---
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
 
 
-# --- Initial Setup ---
-if "session_id" not in st.session_state:
-    st.session_state.session_id = str(uuid.uuid4())
-
-if "rag_sources" not in st.session_state:
-    st.session_state.rag_sources = []
-
-if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "user", "content": "Hello"},
-        {"role": "assistant", "content": "Hi there! How can I assist you today?"}
-]
+out = authenticator.login('main')
 
 
-# --- Side Bar LLM API Tokens ---
-with st.sidebar:
-    if "AZ_OPENAI_API_KEY" not in os.environ:
-        default_openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
-        with st.popover("🔐 OpenAI"):
-            openai_api_key = st.text_input(
-                "Introduce your OpenAI API Key (https://platform.openai.com/)", 
-                value=default_openai_api_key,
-                type="password",
-                key="openai_api_key",
-            )
+if st.session_state['authentication_status'] is False:
+    st.error('Username/password is incorrect')
+if st.session_state['authentication_status'] is None:
+    st.warning('Please enter your username and password')
 
-        default_anthropic_api_key = os.getenv("ANTHROPIC_API_KEY") if os.getenv("ANTHROPIC_API_KEY") is not None else ""
-        with st.popover("🔐 Anthropic"):
-            anthropic_api_key = st.text_input(
-                "Introduce your Anthropic API Key (https://console.anthropic.com/)", 
-                value=default_anthropic_api_key,
-                type="password",
-                key="anthropic_api_key",
-            )
+if st.session_state['authentication_status']:
+    authenticator.logout()
+    # --- Header ---
+    st.html("""<h2 style="text-align: center;">📚🔍 <i> Welcome to LATCHBOT!</i> 🤖💬</h2>""")
+
+
+    # --- Initial Setup ---
+    if "session_id" not in st.session_state:
+        st.session_state.session_id = str(uuid.uuid4())
+
+    if "rag_sources" not in st.session_state:
+        st.session_state.rag_sources = []
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "user", "content": "Hello"},
+            {"role": "assistant", "content": "Hi there! How can I assist you today?"}
+    ]
+
+
+    # --- Side Bar LLM API Tokens ---
+    with st.sidebar:
+        if "AZ_OPENAI_API_KEY" not in os.environ:
+            default_openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
+            with st.popover("🔐 OpenAI"):
+                openai_api_key = st.text_input(
+                    "Introduce your OpenAI API Key (https://platform.openai.com/)", 
+                    value=default_openai_api_key,
+                    type="password",
+                    key="openai_api_key",
+                )
+
+            default_anthropic_api_key = os.getenv("ANTHROPIC_API_KEY") if os.getenv("ANTHROPIC_API_KEY") is not None else ""
+            with st.popover("🔐 Anthropic"):
+                anthropic_api_key = st.text_input(
+                    "Introduce your Anthropic API Key (https://console.anthropic.com/)", 
+                    value=default_anthropic_api_key,
+                    type="password",
+                    key="anthropic_api_key",
+                )
+        else:
+            openai_api_key, anthropic_api_key = None, None
+            st.session_state.openai_api_key = None
+            az_openai_api_key = os.getenv("AZ_OPENAI_API_KEY")
+            st.session_state.az_openai_api_key = az_openai_api_key
+
+
+    # --- Main Content ---
+    # Checking if the user has introduced the OpenAI API Key, if not, a warning is displayed
+    missing_openai = openai_api_key == "" or openai_api_key is None or "sk-" not in openai_api_key
+    missing_anthropic = anthropic_api_key == "" or anthropic_api_key is None
+    if missing_openai and missing_anthropic and ("AZ_OPENAI_API_KEY" not in os.environ):
+        st.write("#")
+        st.warning("⬅️ Please introduce an API Key to continue...")
+
     else:
-        openai_api_key, anthropic_api_key = None, None
-        st.session_state.openai_api_key = None
-        az_openai_api_key = os.getenv("AZ_OPENAI_API_KEY")
-        st.session_state.az_openai_api_key = az_openai_api_key
+        # Sidebar
+        with st.sidebar:
+            st.divider()
+            models = []
+            for model in MODELS:
+                if "openai" in model and not missing_openai:
+                    models.append(model)
+                elif "anthropic" in model and not missing_anthropic:
+                    models.append(model)
+                elif "azure-openai" in model:
+                    models.append(model)
+
+            st.selectbox(
+                "🤖 Select a Model", 
+                options=models,
+                key="model",
+            )
+
+            cols0 = st.columns(2)
+            with cols0[0]:
+                is_vector_db_loaded = ("vector_db" in st.session_state and st.session_state.vector_db is not None)
+                st.toggle(
+                    "Use RAG", 
+                    value=is_vector_db_loaded, 
+                    key="use_rag", 
+                    disabled=not is_vector_db_loaded,
+                )
+
+            with cols0[1]:
+                st.button("Clear Chat", on_click=lambda: st.session_state.messages.clear(), type="primary")
+
+            st.header("RAG Sources:")
+                
+            # File upload input for RAG with documents
+            st.file_uploader(
+                "📄 Upload a document", 
+                type=["pdf", "txt", "docx", "md"],
+                accept_multiple_files=True,
+                on_change=load_doc_to_db,
+                key="rag_docs",
+            )
+
+            # URL input for RAG with websites
+            st.text_input(
+                "🌐 Introduce a URL",
+                placeholder="https://example.com",
+                on_change=load_url_to_db,
+                key="rag_url",
+            )
+
+            with st.expander(f"📚 Documents in DB ({0 if not is_vector_db_loaded else len(st.session_state.rag_sources)})"):
+                st.write([] if not is_vector_db_loaded else [source for source in st.session_state.rag_sources])
 
 
-# --- Main Content ---
-# Checking if the user has introduced the OpenAI API Key, if not, a warning is displayed
-missing_openai = openai_api_key == "" or openai_api_key is None or "sk-" not in openai_api_key
-missing_anthropic = anthropic_api_key == "" or anthropic_api_key is None
-if missing_openai and missing_anthropic and ("AZ_OPENAI_API_KEY" not in os.environ):
-    st.write("#")
-    st.warning("⬅️ Please introduce an API Key to continue...")
+        # Main chat app
+        model_provider = st.session_state.model.split("/")[0]
+        if model_provider == "openai":
+            llm_stream = ChatOpenAI(
+                api_key=openai_api_key,
+                model_name=st.session_state.model.split("/")[-1],
+                temperature=0.3,
+                streaming=True,
+            )
+        elif model_provider == "anthropic":
+            llm_stream = ChatAnthropic(
+                api_key=anthropic_api_key,
+                model=st.session_state.model.split("/")[-1],
+                temperature=0.3,
+                streaming=True,
+            )
+        elif model_provider == "azure-openai":
+            llm_stream = AzureChatOpenAI(
+                azure_endpoint=os.getenv("AZ_OPENAI_ENDPOINT"),
+                openai_api_version="2024-02-15-preview",
+                model_name=st.session_state.model.split("/")[-1],
+                openai_api_key=os.getenv("AZ_OPENAI_API_KEY"),
+                openai_api_type="azure",
+                temperature=0.3,
+                streaming=True,
+            )
 
-else:
-    # Sidebar
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        if prompt := st.chat_input("Your message"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+
+                messages = [HumanMessage(content=m["content"]) if m["role"] == "user" else AIMessage(content=m["content"]) for m in st.session_state.messages]
+
+                if not st.session_state.use_rag:
+                    st.write_stream(stream_llm_response(llm_stream, messages))
+                else:
+                    st.write_stream(stream_llm_rag_response(llm_stream, messages))
+
+
     with st.sidebar:
         st.divider()
-        models = []
-        for model in MODELS:
-            if "openai" in model and not missing_openai:
-                models.append(model)
-            elif "anthropic" in model and not missing_anthropic:
-                models.append(model)
-            elif "azure-openai" in model:
-                models.append(model)
-
-        st.selectbox(
-            "🤖 Select a Model", 
-            options=models,
-            key="model",
-        )
-
-        cols0 = st.columns(2)
-        with cols0[0]:
-            is_vector_db_loaded = ("vector_db" in st.session_state and st.session_state.vector_db is not None)
-            st.toggle(
-                "Use RAG", 
-                value=is_vector_db_loaded, 
-                key="use_rag", 
-                disabled=not is_vector_db_loaded,
-            )
-
-        with cols0[1]:
-            st.button("Clear Chat", on_click=lambda: st.session_state.messages.clear(), type="primary")
-
-        st.header("RAG Sources:")
-            
-        # File upload input for RAG with documents
-        st.file_uploader(
-            "📄 Upload a document", 
-            type=["pdf", "txt", "docx", "md"],
-            accept_multiple_files=True,
-            on_change=load_doc_to_db,
-            key="rag_docs",
-        )
-
-        # URL input for RAG with websites
-        st.text_input(
-            "🌐 Introduce a URL",
-            placeholder="https://example.com",
-            on_change=load_url_to_db,
-            key="rag_url",
-        )
-
-        with st.expander(f"📚 Documents in DB ({0 if not is_vector_db_loaded else len(st.session_state.rag_sources)})"):
-            st.write([] if not is_vector_db_loaded else [source for source in st.session_state.rag_sources])
-
-
-    # Main chat app
-    model_provider = st.session_state.model.split("/")[0]
-    if model_provider == "openai":
-        llm_stream = ChatOpenAI(
-            api_key=openai_api_key,
-            model_name=st.session_state.model.split("/")[-1],
-            temperature=0.3,
-            streaming=True,
-        )
-    elif model_provider == "anthropic":
-        llm_stream = ChatAnthropic(
-            api_key=anthropic_api_key,
-            model=st.session_state.model.split("/")[-1],
-            temperature=0.3,
-            streaming=True,
-        )
-    elif model_provider == "azure-openai":
-        llm_stream = AzureChatOpenAI(
-            azure_endpoint=os.getenv("AZ_OPENAI_ENDPOINT"),
-            openai_api_version="2024-02-15-preview",
-            model_name=st.session_state.model.split("/")[-1],
-            openai_api_key=os.getenv("AZ_OPENAI_API_KEY"),
-            openai_api_type="azure",
-            temperature=0.3,
-            streaming=True,
-        )
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if prompt := st.chat_input("Your message"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-
-            messages = [HumanMessage(content=m["content"]) if m["role"] == "user" else AIMessage(content=m["content"]) for m in st.session_state.messages]
-
-            if not st.session_state.use_rag:
-                st.write_stream(stream_llm_response(llm_stream, messages))
-            else:
-                st.write_stream(stream_llm_rag_response(llm_stream, messages))
-
-
-with st.sidebar:
-    st.divider()
-    # st.video("https://youtu.be/abMwFViFFhI")
-    # st.write("📋[Medium Blog](https://medium.com/@enricdomingo/program-a-rag-llm-chat-app-with-langchain-streamlit-o1-gtp-4o-and-claude-3-5-529f0f164a5e)")
-    # st.write("📋[GitHub Repo](https://github.com/enricd/rag_llm_app)")
+        # st.video("https://youtu.be/abMwFViFFhI")
+        # st.write("📋[Medium Blog](https://medium.com/@enricdomingo/program-a-rag-llm-chat-app-with-langchain-streamlit-o1-gtp-4o-and-claude-3-5-529f0f164a5e)")
+        # st.write("📋[GitHub Repo](https://github.com/enricd/rag_llm_app)")
